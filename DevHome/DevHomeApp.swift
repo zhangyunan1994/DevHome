6452679b1ce017f023581de082d374a4804090e4
yunan.zhang <zyndev@gmail.com>
2024-05-25 16:25:49 +0800

    Initial Commit

---
+++ DevHome/DevHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevHomeApp.swift
+//  DevHome
+//
+//  Created by zhangyunan on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DevHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
